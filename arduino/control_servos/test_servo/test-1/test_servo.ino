4c4c0567e9258f9423da150fcb6009f191996680
vitosaravia <victoria.saravia@estudiantes.utec.edu.uy>
2025-11-03 11:33:41 -0300

    gripper con boton

--- arduino/control_servos/test_servo/test-1/test_servo.ino
+++ arduino/control_servos/test_servo/test-1/test_servo.ino
@@ -1,5 +1,5 @@
 // Este codigo es el primero donde se toma el valor VRx del joystick para mover el servo-1
-// Este codigo es el primero donde se toma el valor VRy del joystick para mover el servo-2
+// falta el control de un servo, que puede ser el de la pinza
 
 
 //Ambos se alimentan con 4 pilas AA que entregan 5V, GND a la placa.
@@ -8,11 +8,11 @@
 
 #include <Servo.h>
 
-Servo servo_1;  //servo_1
-Servo servo_2;  //servo_2
-Servo servo_3;  //servo_3
-Servo servo_4;  //servo_4
-Servo servo_5;  //servo_5
+Servo servo_1;  //servo_1 -> joystick 1 X -> base rotativa
+Servo servo_2;  //servo_2 -> joystick 1 Y -> 1er brazo
+Servo servo_3;  //servo_3 -> joystick 2 X -> 2do brazo
+Servo servo_4;  //servo_4 -> joystick 2 Y -> giro gripper
+Servo servo_5;  //servo_5 -> gripper
 
 int Grados_1 = 0;  // posición servo_1
 int Grados_2 = 0;  // posición servo_2
@@ -24,6 +24,7 @@ int Grados_5 = 0;  // posición servo_5
 #define Joystick1_Y A1  // entrada analogica Joystick1 Y
 #define Joystick2_X A2  // entrada analogica Joystick1 X
 #define Joystick2_Y A3  // entrada analogica Joystick1 Y
+#define button_gripper 8  // Pin donde está conectado el botón que activa el gripper
 
 void setup() {
   Serial.begin(9600);
@@ -32,6 +33,8 @@ void setup() {
   servo_3.attach(10);   // salida pwm pin 10
   servo_4.attach(11);   // salida pwm pin 11
   servo_5.attach(5);   // salida pwm pin 5
+
+  pinMode(gripper, INPUT);
 }
 
 void loop() {
@@ -53,12 +56,25 @@ void loop() {
   if (Joystick2_Y_valor < 340) Grados_4 += 3;
   else if (Joystick2_Y_valor > 680) Grados_3 -= 3;
 
+  // Rangos de los servos
   Grados_1 = min(180, max(0, Grados_1));  // Angulos maximos y minimos de servo 1
   Grados_2 = min(180, max(0, Grados_2));  // Angulos maximos y minimos de servo 2
 
   Grados_3 = min(180, max(0, Grados_3));  // Angulos maximos y minimos de servo 3
   Grados_4 = min(180, max(0, Grados_4));  // Angulos maximos y minimos de servo 4
+
+
+  // Leer el botón
+  int estadoGripper = digitalRead(gripper);
+
+  // Si el botón se presiona (LOW), mover el servo_5 a 60°
+  if (estadoGripper == LOW) {
+    Grados_5 = 60;
+  } else {
+    Grados_5 = 0;
+  }
  
+    // Monitorear por Serial
   Serial.print("Grados eje 1 : ");
   Serial.println(Grados_1); 
   Serial.print("Grados eje 2 : ");
@@ -67,11 +83,14 @@ void loop() {
   Serial.println(Grados_3); 
   Serial.print("Grados eje 4 : ");
   Serial.println(Grados_4);  
+  Serial.print("Grados eje 5: ");
+  Serial.println(Grados_5);
   Serial.print("\n");
   
   servo_1.write(Grados_1);
   servo_2.write(Grados_2);
   servo_3.write(Grados_3);
   servo_4.write(Grados_4);
+  servo_4.write(Grados_5);
   
 }
